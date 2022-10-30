7dcbb429d8a842d8fd68bfdd0c409a6d5cb8803f
chernousovaea <115497090+chernousovaea@users.noreply.github.com>
2022-10-30 23:11:36 +0300

    Лабораторная работа 4

---
+++ Лабораторная работа 4/task 3.py
@@ -0,0 +1,13 @@
+def delete(list_, index=None):
+    if index is not None:
+        left_list = list_[:index]
+        right_list = list_[index+1:]
+        new_list = left_list + right_list
+    else:
+        new_list = list_[:len(list_)-1]
+
+    return new_list
+
+print(delete([0, 0, 1, 2], index=0))  # [0, 1]
+print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3]
